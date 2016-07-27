2b5d8cfcddb077009b6afa71c8dbc2236d008bf1
periyasamyp <periyasamy753@gmail.com>
2016-07-27 15:23:44 +0530

    Create pp.java

---
+++ pp.java
@@ -0,0 +1,11 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+ int n;
+    printf("enter the number");
+    scanf("%d",&nu);
+    printf("reverse is %d",reverse(n));
+    getchar();
+    getch();
+}
